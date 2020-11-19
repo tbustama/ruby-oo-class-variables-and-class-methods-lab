d41b5a56134a7f46815b9fd59bf3bd35508fb86e
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-18 16:28:58 -0800

    Done

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << @genre
+        @@artists << @artist
+    end
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        hash = Hash.new(0)
+        @@genres.each do |ele|
+            hash[ele] += 1
+        end
+        hash
+    end
+    def self.artist_count
+        hash = Hash.new(0)
+        @@artists.each do |ele|
+            hash[ele] += 1
+        end
+        hash
+    end
+end
